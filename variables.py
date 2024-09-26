1e282ef65e7947728492032517102a0d92ed7aec
Jalynn Bolds <boldsjal000@warren.k12.in.us>
2024-09-26 07:29:50 -0400

    Process finished with exit code 0

---
+++ variables.py
@@ -0,0 +1,7 @@
+num_apples = 20
+num_oranges = 15
+num_oranges1 = 0
+print("Number of apples:", num_apples)
+print("Number of oranges:", num_oranges)
+print("Number of apples:", num_apples)
+print("Number of oranges:", num_oranges1)
